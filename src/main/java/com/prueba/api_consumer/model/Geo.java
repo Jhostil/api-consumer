9505520b571b694676f37a6fe5579e55fac7af7a
Jhoiner <jhooiineer@gmail.com>
2024-09-17 18:53:00 -0500

    Se crean las entidades

---
+++ src/main/java/com/prueba/api_consumer/model/Geo.java
@@ -0,0 +1,15 @@
+package com.prueba.api_consumer.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Geo {
+
+    private String lat;
+    private String lng;
+
+}
---
+++ src/main/java/com/prueba/api_consumer/model/User.java
@@ -0,0 +1,22 @@
+package com.prueba.api_consumer.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private int id;
+    private String name;
+    private String username;
+    private String email;
+    private Address address;
+    private String phone;
+    private String website;
+    private Company company;
+
+
+}
