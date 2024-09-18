cc027523add8a0ab0b41c1dddb142f3846ce6180
Jhoiner <jhooiineer@gmail.com>
2024-09-17 19:03:53 -0500

    Se crea el End Point para obtener los usuarios

---
+++ src/main/java/com/prueba/api_consumer/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.prueba.api_consumer.controller;
+
+import com.prueba.api_consumer.model.User;
+import com.prueba.api_consumer.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    public List<User> getUsers(){
+
+        return userService.getUsers();
+    }
+}
